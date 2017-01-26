20864c414c017647b7d0dd2154dfe4bd3f1c556a
3LP <3levelproductions@gmail.com>
2017-01-25 17:35:37 -0800

    second commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes/routes';
-import {browserHistory} from 'react-router';
-
-
-
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
-  <Routes history={browserHistory}/>,
+  <App />,
   document.getElementById('root')
 );
