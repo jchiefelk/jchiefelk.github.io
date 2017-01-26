aed5f06efaf5e2e7e27391a9edfd0d6267180479
3LP <3levelproductions@gmail.com>
2017-01-26 08:43:52 -0800

    Portfolio Update

--- server/app.js
+++ server/app.js
@@ -57,7 +57,7 @@ app.post('/api', (req, res, next) => {
 });
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'index.html'));
 });
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
